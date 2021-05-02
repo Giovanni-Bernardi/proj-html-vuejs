7f0ae79566d6fe2275d0831937d3c0b5a53dd4a1
Giovanni <giovanniberanrdi13@gmail.com>
2021-05-02 12:09:28 +0200

    parallax effect

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -158,7 +158,7 @@ function initVue() {
       },
       validEmail: function (email) {
         var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
+                return re.test(email);
       },
     },
   });
