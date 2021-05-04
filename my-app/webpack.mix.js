bd38e104eb3fb4470563bfd07c8912d20a8d8001
Giovanni <giovannibernardi13@gmail.com>
2021-05-04 19:55:16 +0200

    webpack.mix.js fix

--- my-app/webpack.mix.js
+++ my-app/webpack.mix.js
@@ -2,11 +2,6 @@ let mix = require('laravel-mix');
 mix
     .js('src/app.js', 'dist')
     .sass('src/style.scss', '')
+    .options({processCssUrls: false,})
     .setPublicPath('dist')
     .copy('src/index.html', 'dist')
-
-    mix.options({
-        // Don't perform any css url rewriting by default
-        processCssUrls: false,
-    })
-;
